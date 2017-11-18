b3b84617dd1aadb78d798bb0a8e48a473d6538e5
fsorodrigues <desouzaoliveirarod.f@husky.neu.edu>
2017-11-18 13:52:41 -0500

    3rd iteration
    styling circles
    fixing scale

--- scatterplot/index.js
+++ scatterplot/index.js
@@ -1,6 +1,6 @@
 var width = 700;
 var height = 700;
-var radius = 4;
+var radius = 6;
 
 var margin = { "top": 50,
                "bottom": 50,
@@ -19,7 +19,9 @@ var tooltip = d3.select("body")
                  .attr("class", "tooltip")
                  .style("opacity", 0);
 
-var formatDecimalComma = d3.format(",.2f")
+var formatDecimalComma = d3.format(",.2f");
+
+var colorScale;
 
 d3.csv("beach_nour_clean.csv", function (dataIn) {
 
@@ -32,12 +34,30 @@ d3.csv("beach_nour_clean.csv", function (dataIn) {
 
   console.log(dataIn);
 
+  var nested = d3.nest()
+                  .key( function(d) { return d.fund_source })
+                  .entries(dataIn);
+
+  console.log(nested);
+
+  var fundMap = nested.map( function(d) { return d.key }).sort(d3.ascending);
+
+  console.log(fundMap);
+
+  var cats = fundMap.length;
+
+  // var colorScheme = d3.schemeRdBu[cats];
+  var colorScheme = d3.schemePuOr[cats];
+
+
+  colorScale = d3.scaleOrdinal(colorScheme).domain(fundMap);
+
   // setting dynamic scales for axis
   var maxX = getMaxX(dataIn);
   console.log(maxX)
   scaleX = d3.scaleLinear()
                .domain([0, maxX])
-               .range([0, 700])
+               .range([0, 600])
                .nice(); // making scale end in round number
 
   var maxY = getMaxY(dataIn);
@@ -52,6 +72,42 @@ d3.csv("beach_nour_clean.csv", function (dataIn) {
   yAxis(scaleY);
   //
   drawPlot(dataIn);
+
+  var legend = svg.append("g")
+                  .attr("class", "legend")
+                  .attr("transform", "translate(" + 520 + "," + 15 + ")")
+
+  var legendTitle = legend.append("g")
+                          .attr("transform", "translate(-8,0)")
+                          .append("text")
+                          .attr("class", "legend-title")
+                          .text("Funding type")
+
+  var legendCircle = legend.append("g")
+                           .attr("transform", "translate(0,15)")
+                           .selectAll(".legend-circle")
+                           .data(fundMap)
+                           .enter()
+                           .append("circle")
+                           .attr("class", "legend-circle")
+                           .attr("cx", 0)
+                           .attr("cy", function(d,i) { return i * 20 })
+                           .attr("r", radius)
+                           .attr("fill", function(d) { return colorScale(d); })
+                           .attr("fill-opacity", 0)
+                           .attr("stroke", function(d) { return colorScale(d) })
+                           .attr("stroke-width", 1.5);
+
+   var legendLabel = legend.append("g")
+                            .attr("transform", "translate(0,15)")
+                            .selectAll(".legend-label")
+                           .data(fundMap)
+                           .enter()
+                           .append("text")
+                           .attr("class", "legend-label")
+                           .attr("x", 12)
+                           .attr("y", function(d,i) { return 5 + (i * 20) })
+                           .text(function(d) { return d; })
 
 });
 
@@ -76,9 +132,12 @@ function drawPlot(dataset) {
                  .attr("cx", function(d) { return scaleX(d.cost_2013) })
                  .attr("cy", function(d) { return scaleY(d.volume) })
                  .attr("r", radius)
-                 .attr("fill", "red")
+                 .attr("fill", function(d) { return colorScale(d.fund_source) })
+                 .attr("stroke", function(d) { return colorScale(d.fund_source) })
+                 .attr("stroke-width", 1.5)
+                 .attr("fill-opacity", 0)
                 //  .attr("fill", function(d) { return makeColor(d.hrsNum, (d.gamesAgainst/2)) })
-                 .attr("opacity", .2);
+                 .attr("opacity", 1);
 
       scatterPlot.enter()
                   .append("circle")
@@ -91,9 +150,12 @@ function drawPlot(dataset) {
                     .duration(500)
                     .ease(d3.easeSin)
                   .attr("r", radius)
-                  .attr("fill", "red")
+                  .attr("fill", function(d) { return colorScale(d.fund_source) })
+                  .attr("stroke", function(d) { return colorScale(d.fund_source) })
+                  .attr("stroke-width", 1.5)
+                  .attr("fill-opacity", 0)
                   // .attr("fill", function(d) { return makeColor(d.hrsNum, (d.gamesAgainst/2)) })
-                  .attr("opacity", .2);
+                  .attr("opacity", 1);
 
       scatterPlot.exit()
                   .transition()
@@ -146,6 +208,9 @@ function mouseOver(d) {
                            + "<br>$/CY: " + formatDecimalComma((d.cost_2013 / d.volume)) )
                 .style("left", (d3.event.pageX + 10) + "px")
                 .style("top", (d3.event.pageY - 28) + "px");
+
+        d3.select(this).attr("fill-opacity", 1);
+
               };
 
 function mouseOut() {
@@ -153,5 +218,7 @@ function mouseOut() {
                .duration(50)
                .style("opacity", 0);
 
-        tooltip.html("")
+        tooltip.html("");
+
+        d3.select(this).attr("fill-opacity", 0);
                            };
